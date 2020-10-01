5e4d9fadb27857f0375ed056201a58bb46bc9aab
AA <aa@bigdumb.gq>
2020-10-01 16:50:21 -0400

    fix it, also lint

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
-
-
-const { Plugin } = require('powercord/entities')
-const { inject, uninject } = require('powercord/injector')
-const { forceUpdateElement } = require('powercord/util')
+const { Plugin } = require('powercord/entities');
+const { inject, uninject } = require('powercord/injector');
+const { forceUpdateElement } = require('powercord/util');
 const { getModule, constants: { Permissions } } = require('powercord/webpack');
 
 module.exports = class HiddenChannels extends Plugin {
-
-	async startPlugin() {
+  async startPlugin () {
     const permissionsModule = await getModule([ 'can' ]);
     inject('channels-perms', permissionsModule, 'can', (args, res) => {
-      if (res = true) {
-        return args[0].data === Permissions.VIEW_CHANNEL.data || 
+      if (res === false) {
+        return args[0].data === Permissions.VIEW_CHANNEL.data ||
                args[0].data === Permissions.SEND_MESSAGES.data ||
                args[0].data === Permissions.READ_MESSAGE_HISTORY.data ||
                args[0].data === Permissions.CONNECT.data ||
                args[0].data === Permissions.USE_VAD.data ||
                args[0].data === Permissions.SPEAK.data;
-        
       }
       return res;
     });
     const { containerDefault } = await getModule([ 'containerDefault' ]);
     forceUpdateElement(`.${containerDefault}`, true);
-	}
+  }
 
-	pluginWillUnload() {
+  pluginWillUnload () {
     uninject('channels-perms');
 
     const classes = getModule([ 'containerDefault' ], false);
     if (classes) {
       forceUpdateElement(`.${classes.containerDefault}`, true);
     }
-	}
-}
+  }
+};
