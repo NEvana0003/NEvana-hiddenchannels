8f859cb2903455747f745fbc216f1cc76293247d
NEvana <54038645+NEvana0003@users.noreply.github.com>
2020-10-01 16:15:52 -0400

    initial

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+const { Plugin } = require('powercord/entities')
+const { inject, uninject } = require('powercord/injector')
+const { forceUpdateElement } = require('powercord/util')
+const { getModule, constants: { Permissions } } = require('powercord/webpack');
+
+module.exports = class HiddenChannels extends Plugin {
+
+	async startPlugin() {
+    const permissionsModule = await getModule([ 'can' ]);
+    inject('channels-perms', permissionsModule, 'can', (args, res) => {
+      if (res = true) {
+        return args[0].data === Permissions.VIEW_CHANNEL.data || 
+               args[0].data === Permissions.SEND_MESSAGES.data ||
+               args[0].data === Permissions.READ_MESSAGE_HISTORY.data;
+        
+      }
+      return res;
+    });
+    const { containerDefault } = await getModule([ 'containerDefault' ]);
+    forceUpdateElement(`.${containerDefault}`, true);
+	}
+
+	pluginWillUnload() {
+    uninject('channels-perms');
+
+    const classes = getModule([ 'containerDefault' ], false);
+    if (classes) {
+      forceUpdateElement(`.${classes.containerDefault}`, true);
+    }
+	}
+}
